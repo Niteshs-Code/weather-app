4e15bbb36f096d6d88d4b2770de167d383c31f84
Niteshs-Code <niteshkumar29946@gmail.com>
2025-10-07 11:21:05 +0530

    fontsize chante

--- src/components/Good.jsx
+++ src/components/Good.jsx
@@ -1,7 +1,7 @@
 const Good = [
   { id: 1, url: "https://www.pexels.com/download/video/2941521//" },
   { id: 2, url: "https://www.pexels.com/download/video/31813651/" },
-  { id: 3, url: "https://www.pexels.com/download/video/3105317/" },
+  { id: 3, url: "https://www.pexels.com/download/video/19807702/" },
   { id: 4, url: "https://www.pexels.com/download/video/32395004/" },
   { id: 5, url: "https://www.pexels.com/download/video/3786014/" },
   { id: 6, url: "https://www.pexels.com/download/video/2758322/" },
@@ -9,6 +9,7 @@ const Good = [
   { id: 8, url: "https://www.pexels.com/download/video/9113144/" },
   { id: 9, url: "https://www.pexels.com/download/video/3214449/" },
   { id: 10, url: "https://www.pexels.com/download/video/5737314/" },
+  { id: 11, url: "https://www.pexels.com/download/video/7046816/" },
 ];
 
 export default Good;
